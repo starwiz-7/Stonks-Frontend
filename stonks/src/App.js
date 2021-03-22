4e6f3d6300ef76c2510a1862550ac34dc4727f76
starwiz-7 <aryan2019@iiitkottayam.ac.in>
2021-03-22 19:25:27 +0530

    build: Setup codebase-2

--- stonks/src/App.js
+++ stonks/src/App.js
@@ -1,25 +1,21 @@
+import React, {Suspense} from 'react';
+import {Switch, Route, Redirect, BrowserRouter} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
+import Login from './Screens/Login/login';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  let routes = (
+    <BrowserRouter>
+    <Switch>
+      <Route path="/" render={(props) => <Login />}/>
+    </Switch>
+    </BrowserRouter>
   );
+  
+  return <Suspense fallback={<p>Loading ...</p>}>{routes}</Suspense>;
 }
 
 export default App;
